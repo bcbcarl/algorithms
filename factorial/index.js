e94fda8fada85061c7f5f5edaa72be7ae431c420
Carl X. Su <bcbcarl@gmail.com>
2018-03-28 18:06:26 +0800

    Add iterative factorial

--- factorial/index.js
+++ factorial/index.js
@@ -2,10 +2,17 @@ const f = (n) => n > 1
   ? f(n - 1) * n
   : 1;
 
+const f_iter = (n) => {
+  let s = 1;
+  for (let i = 1; i <= n; ++i) {
+    s *= i;
+  }
+  return s;
+};
 
 const main = () => {
   for (let i = 0; i <= 10; ++i) {
-    console.log(`f(${i}): ${f(i)}`);
+    console.log(`f(${i}): ${f(i)}, f_iter(${i}): ${f_iter(i)}`);
   }
 };
 
