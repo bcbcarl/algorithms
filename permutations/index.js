5fd4a21fc6889351c7cc231bf1691aa759d027bb
Carl X. Su <bcbcarl@gmail.com>
2018-04-03 15:10:28 +0800

    Add permutations

---
+++ permutations/index.js
@@ -0,0 +1,29 @@
+const perm = (list) => {
+  let result = [];
+
+  const generate = (n, list) => {
+    if (1 === n) {
+      result.push(list.slice());
+      return;
+    }
+
+    for (let i = 0; i < n - 1; ++i) {
+      generate(n - 1, list);
+      (n % 2)
+      ? [list[0], list[n - 1]] = [list[n - 1], list[0]]
+      : [list[i], list[n - 1]] = [list[n - 1], list[i]];
+    }
+    generate(n - 1, list);
+  };
+
+  generate(list.length, list);
+  return result;
+};
+
+const main = () => {
+  const list = [1, 2, 3];
+  const result = perm(list);
+  console.log(result);
+};
+
+main();
